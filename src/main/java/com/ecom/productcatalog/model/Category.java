33f96f76605a8a846514013dc660ad5c5acf5af1
Sudarshan <sudarshanshinde033@gmail.com>
2026-08-19 23:03:27 +0530

    git reset

--- src/main/java/com/ecom/productcatalog/model/Category.java
+++ src/main/java/com/ecom/productcatalog/model/Category.java
@@ -17,7 +17,7 @@ public class Category {
     private Long id;
     private String Sudarshanshinde;
     private String name;
-
+    private String resetTest1;
     
     @OneToMany(mappedBy = "category",
             cascade = CascadeType.ALL,
